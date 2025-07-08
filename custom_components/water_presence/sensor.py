89f2a99795f21d071e281c26ca874efaed0949b5
Riley <rs.parkerv@proton.me>
2025-07-08 21:41:18 +1000

    bump version to 0.0.10 in manifest.json and refactor sensor logic to remove device status tracking

--- custom_components/water_presence/sensor.py
+++ custom_components/water_presence/sensor.py
@@ -5,7 +5,6 @@ from homeassistant.helpers.event import async_track_state_change_event
 from homeassistant.helpers.restore_state import RestoreEntity
 from .const import DOMAIN
 
-DEVICE_STATUS_ENTITY = "switch.kettle_power"  # Update if your power entity is different
 TEMPERATURE_ENTITY = "sensor.smart_kettle_current_temperature"
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
@@ -22,7 +21,7 @@ class WaterPresenceSensor(SensorEntity, RestoreEntity):
     def __init__(self, hass):
         self.hass = hass
         self._state = None
-        self._history = []  # Store tuples of (status, temp, time)
+        self._history = []  # Store tuples of (temp, time)
 
     @property
     def state(self):
@@ -30,29 +29,28 @@ class WaterPresenceSensor(SensorEntity, RestoreEntity):
 
     async def async_added_to_hass(self):
         await super().async_added_to_hass()
-        async_track_state_change_event(self.hass, [DEVICE_STATUS_ENTITY, TEMPERATURE_ENTITY], self._async_state_changed)
+        async_track_state_change_event(self.hass, [TEMPERATURE_ENTITY], self._async_state_changed)
         # Restore previous state if available
         old_state = await self.async_get_last_state()
         if old_state:
             self._state = old_state.state
 
     async def _async_state_changed(self, event):
-        status = self.hass.states.get(DEVICE_STATUS_ENTITY)
         temp = self.hass.states.get(TEMPERATURE_ENTITY)
-        if status and temp:
+        if temp:
             try:
                 temp_val = float(temp.state)
             except ValueError:
                 temp_val = None
-            self._history.append((status.state, temp_val, event.time_fired))
+            self._history.append((temp_val, event.time_fired))
             # Keep only the last 10 records
             self._history = self._history[-10:]
             self._state = self._analyze_history()
             self.async_write_ha_state()
 
     def _analyze_history(self):
-        # Simple algorithm: if temperature rises when device is on, assume water present
-        for status, temp, _ in reversed(self._history):
-            if status == "on" and temp is not None and temp > 40:  # 40°C as a threshold
+        # If temperature is above 40°C, assume water is present
+        for temp, _ in reversed(self._history):
+            if temp is not None and temp > 40:
                 return "full"
         return "empty"
