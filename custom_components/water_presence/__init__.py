fd887ad692fcbd206720b1fab5fb0384310b3b8c
Riley <rs.parkerv@proton.me>
2025-07-08 21:08:19 +1000

    update async_setup_entry to use async_forward_entry_setups and bump version to 0.0.8 in manifest.json

--- custom_components/water_presence/__init__.py
+++ custom_components/water_presence/__init__.py
@@ -11,7 +11,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Water Presence from a config entry."""
-    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
